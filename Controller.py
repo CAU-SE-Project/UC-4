b5eb8912910aeaba6694be912ea4cd4f016cf143
es3442 <77157003+es3442@users.noreply.github.com>
2021-05-30 22:07:55 +0900

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,60 +1,44 @@
-import sys
-from PyQt5.QtWidgets import (QWidget, QGridLayout,QPushButton, QApplication)
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
 import DiseaseFinder
 import DiseaseSelector
-import DevCtrl
 
-class Controller:
-    def __init__(self, symptomlist):
-        self.symptomlist = symptomlist
-        DiseaseList, self.tempDiseaseFinder = self.findDisease(symptomlist)
-        app = QApplication(sys.argv)
-        self.windowExample = DevCtrl.DevCtrl(symptomlist, DiseaseList, self)
-        self.windowExample.show()
-        sys.exit(app.exec_())
+# enter symptoms() 함수를 이용해서 받아오지만, 일부분만 구현했기 때문에 symptomlist에 특정 값을 넣어놓았다.
+symptomlist = ['등', '관절통']
+val = DiseaseFinder.DiseaseFinder(symptomlist).checkDisease()
+back = 0  # back이 1이면 backbutton이 활성화되었다고 생각하고 그 전 단계로 돌아간다.
+if val != None:
+    # create diseaseList
+    DiseaseList = []  # 병명, site 주소가 저장되어 있다.
+    # destory symptom's list
+    del(symptomlist)
+    for key in val:  # 이름만 따로 저장해서 activate("viewer, DiseaseList")
+        DiseaseList.append(key)
 
-    def findDisease(self, symptomlist):
-        DiseaseList = []
-        tempDiseaseFinder = DiseaseFinder.DiseaseFinder(symptomlist).checkDisease() #disease name과 url을 가짐
-        for key in tempDiseaseFinder:
-            DiseaseList.append(key)
-        print(DiseaseList)
-        return DiseaseList, tempDiseaseFinder
+    ###activate("viewer", d)
+    print(DiseaseList)
 
-    def selectDisease(self, selectedDisease):
-        #selectedDisease = '라이 증후군(Reye syndrome)'
-        url = ""
-        for diseaseName in self.tempDiseaseFinder:
-            if(selectedDisease == diseaseName):
-                url = self.tempDiseaseFinder[diseaseName]
+    # if back==1:
+    # activate backbutton + prompt
+    # enter symptoms()
 
-        tempDiseaseInfo = DiseaseSelector.DiseaseSelector(url).makeSelectedDiseaseInfo()
-        self.windowExample2 = DevCtrl.DevCtrl2(selectedDisease, tempDiseaseInfo)
-        self.windowExample2.show()
+    # enter selectedDisease()
+    selectedDisease = '간염(Hepatitis)'
+    url = url = val[selectedDisease]
 
-        selectedDiseaseInfo = {}  # 병명, 진료과 저장
-        selectedDiseaseInfo['병명'] = selectedDisease
-        for diseaseinfo in tempDiseaseInfo:
-            if diseaseinfo == "진료과":
-                selectedDiseaseInfo[diseaseinfo] = tempDiseaseInfo[diseaseinfo]
-        
-        f = open("selectedDiseaseInfo.txt", "w")
-        keys = list(selectedDiseaseInfo.keys())
-        for k in keys:
-            f.write(str(k)+": "+selectedDiseaseInfo[k]+"\n")
-        f.close()
+    val = DiseaseSelector.DiseaseSelector(
+        url).makeSelectedDiseaseInfo()  # 선택된 증상, 관련질환, 진료과 등등이 저장
+    del(DiseaseList)
+    selectedDiseaseInfo = {}  # 병명, 진료과 저장
+    selectedDiseaseInfo['병명'] = selectedDisease
+    for diseaseinfo in val:
+        if diseaseinfo == "진료과":  # 병명과 진료과만 따로 저장(이후에 UC-5, 6에서 이용하기 위해)
+            selectedDiseaseInfo[diseaseinfo] = val[diseaseinfo]
+    ###activate("viewer", selectedDiseaseInfo)
+    print("selecteDiseaseInfo", selectedDiseaseInfo)
+    # if back==1:
+    # activate backbutton + prompt
+    # enter selectedDisease()
 
-        #print(diseaseinfo)
-        print(tempDiseaseInfo)
-        print(tempDiseaseInfo.keys())
-        return selectedDiseaseInfo
-
-
-DiseaseList = []
-symptomlist = ['건망증']
-controller = Controller(symptomlist)
-
-
+else:
+    # activate backbutton + prompt
+    print("decrease symptoms")
+    # enter symptoms()
